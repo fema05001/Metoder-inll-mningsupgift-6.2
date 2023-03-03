6610fb84934972d8b2aab1903556d13389758e4d
Elev <Elev@TEB125102>
2023-03-03 14:10:41 +0100

    Add project files.

---
+++ Metoder inllämningsupgift 6.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace inllämningsuppgift
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(störst(9,7));
+          
+            Console.WriteLine(störst(5,8));
+           
+            Console.WriteLine(störst(6,6));
+        }
+        static int störst(int tal1, int tal2)
+        {
+            if (tal1 == tal2)
+            {
+                return tal1;
+            }
+            else if (tal1 > tal2)
+            {
+                return tal1;
+                
+            }
+            else
+                return tal2;
+        }
+    }
+}
